ed6176150e4947280084bf6e4f942d19e4def452
James Ward <james@jamesward.com>
2018-03-22 15:14:22 -0600

    fix tests with new filters

--- test/utils/OnlyHttpsFilterSpec.scala
+++ test/utils/OnlyHttpsFilterSpec.scala
@@ -11,13 +11,14 @@ import play.api.mvc._
 import play.api.test.Helpers._
 import play.api.test._
 
-class FiltersSpec extends MixedPlaySpec {
+class OnlyHttpsFilterSpec extends MixedPlaySpec with HeaderNames {
 
   def defaultApp = DAOMock.noDatabaseAppBuilder().build()
 
-  "Filters" must {
+  "OnlyHttpsFilter" must {
     "redirect to https if the request was forwarded and not https" in new App(defaultApp) {
-      val Some(result) = route(app, FakeRequest(GET, controllers.routes.Application.index().url, Headers(HeaderNames.X_FORWARDED_PROTO -> "http"), AnyContentAsEmpty))
+      val request = FakeRequest(GET, controllers.routes.Application.index().url, Headers(X_FORWARDED_PROTO -> "http", HOST -> "localhost"), AnyContentAsEmpty)
+      val Some(result) = route(app, request)
       status(result) mustEqual MOVED_PERMANENTLY
     }
     "not force https for well-known requests" in new App(defaultApp) {
@@ -29,7 +30,8 @@ class FiltersSpec extends MixedPlaySpec {
       status(result) mustEqual OK
     }
     "keep https for non-well-known requests" in new App(defaultApp) {
-      val Some(result) = route(app, FakeRequest(GET, controllers.routes.Assets.at().url, Headers(HeaderNames.X_FORWARDED_PROTO -> "https"), AnyContentAsEmpty))
+      val request = FakeRequest(GET, controllers.routes.Assets.at().url, Headers(X_FORWARDED_PROTO -> "https", HOST -> "localhost"), AnyContentAsEmpty)
+      val Some(result) = route(app, request)
       status(result) mustEqual OK
     }
     "return the well known value when the WELL_KNOWN env var is set" in { () =>
