18f0ad4a6c6e83c293dd66c56410bc02bbf958db
James Ward <james@jamesward.com>
2018-08-23 18:10:14 -0600

    sort tasks by id since those are sequential and auto-task creation results in incorrect sorting with create date

--- app/modules/DAOModule.scala
+++ app/modules/DAOModule.scala
@@ -240,7 +240,7 @@ class DAOWithCtx @Inject()(database: DatabaseWithCtx)(implicit ec: ExecutionCont
       run {
         quote {
           for {
-            task <- query[Task].filter(_.requestSlug == lift(requestSlug)).sortBy(_.createDate)
+            task <- query[Task].filter(_.requestSlug == lift(requestSlug)).sortBy(_.id)
             numComments = query[Comment].filter(_.taskId == task.id).size
           } yield task -> numComments
         }
