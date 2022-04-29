b74f38a09597adaf61b134d9e74e9ddc9f03e7e5
Jim Jagielski <jjagielski@salesforce.com>
2022-04-29 07:40:43 -0400

    Typo fix

--- app/modules/AuthModule.scala
+++ app/modules/AuthModule.scala
@@ -220,7 +220,7 @@ class SamlAuth @Inject() (configuration: Configuration, wsClient: WSClient) (imp
         val samlResponse = new SamlResponse(settings(metadata), httpRequest)
 
         Future.fromTry(Try(samlResponse.checkStatus())).flatMap { _ =>
-          samlResponse.getNameId.fold(Future.failed[Set[String]](new Exception("No emails"))) { emails =>
+          samlResponse.getNameId().fold(Future.failed[Set[String]](new Exception("No emails"))) { emails =>
             Future.successful(emails.asScala.toSet)
           }
         }
